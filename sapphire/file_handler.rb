f22fb89c0590e01773bc7f5ce10764193260902f
Amit Acharya <amit@nestaway.com>
2016-07-06 12:25:25 +0530

    File handler in ruby, methods to know

--- sapphire/file_handler.rb
+++ sapphire/file_handler.rb
@@ -1,7 +1,16 @@
 class FileHandler
   def check_if_file_exists?
     filename = __FILE__
-    File.exists?(filename)
+    call_other_methods_on_file(filename) if File.exists?(filename)
+  end
+
+  def call_other_methods_on_file(fn)
+    puts "Basename: #{File.basename(fn)}"
+    puts "expand_path: #{File.expand_path(fn)}"
+    puts "dirname: #{File.dirname(fn)}"
+    puts "extname: #{File.extname(fn)}"
+    puts "mtime: #{File.mtime(fn)}"
+    puts "size: #{File.size(fn)} bytes"
   end
 end
 puts FileHandler.new.check_if_file_exists?
