6de2e1200dd27c2c863cb992f6ebc6a23055ba06
Amit Acharya <amit@nestaway.com>
2016-07-06 11:47:01 +0530

    precedence check for and and &&

--- not.rb
+++ not.rb
@@ -3,4 +3,8 @@ bday = 'Monday'
 working_overtime = false
 
 if aday == 'Sunday' and bday == 'Monday' and not working_overtime then puts 'Thats right' else puts 'Thats not right' end
-if aday == 'Sunday' and bday == 'Monday' && !working_overtime then puts 'Thats right' else puts 'Thats not right' end
+#=> Thats right
+if aday == 'Sunday' || bday == 'Monday' && !working_overtime then puts 'Thats right' else puts 'Thats not right' end
+#=> Thats right
+if aday == 'Sunday' and bday == 'Monday' && working_overtime then puts 'Thats right' else puts 'Thats not right' end
+#=> Thats not right
