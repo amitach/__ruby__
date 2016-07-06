e9d2012b3f6751bae35a0eb0bb66f106420e4d6c
Amit Acharya <amit@nestaway.com>
2016-07-06 11:56:39 +0530

    switch case styles using range

--- switch.rb
+++ switch.rb
@@ -11,5 +11,37 @@ def show_day(number)
   end
 end
 
+
+def show_day_another_way(number)
+  case number
+  when 1 then puts 'Monday'
+  when 2 then puts 'Tuesday'
+  when 3 then puts 'Wednesday'
+  when 4 then puts 'Thursday'
+  when 5 then puts 'Friday'
+  when 6,7 then puts 'Hurray! Weekend'
+#  when 7 then puts 'Sunday'
+  else puts 'Thats not a day!'
+  end
+end
+
+def show_day_yet_another_way(number)
+  case number
+  when 1 then puts 'Monday'
+  when 2 then puts 'Tuesday'
+  when 3 then puts 'Wednesday'
+  when 4 then puts 'Thursday'
+  when 5 then puts 'Friday'
+  when (6..7) then puts 'Hurray! Weekend'
+#  when 7 then puts 'Sunday'
+  else puts 'Thats not a day!'
+  end
+end
+
 (1..8).to_a.each{ |number| show_day(number) }
+puts '*' * 100
+(1..8).to_a.each{ |number| show_day_another_way(number) }
+puts '*' * 100
+(1..8).to_a.each{ |number| show_day_yet_another_way(number) }
+puts '*' * 100
 
