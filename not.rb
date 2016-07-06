fca227aa58b9bd24998b54d943b689cf216c42ce
Amit Acharya <amit@nestaway.com>
2016-07-06 11:44:22 +0530

    one more style

--- not.rb
+++ not.rb
@@ -3,3 +3,4 @@ bday = 'Monday'
 working_overtime = false
 
 if aday == 'Sunday' and bday == 'Monday' and not working_overtime then puts 'Thats right' else puts 'Thats not right' end
+if aday == 'Sunday' and bday == 'Monday' && !working_overtime then puts 'Thats right' else puts 'Thats not right' end
