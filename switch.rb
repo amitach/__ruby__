24d244227a009c4a806f65bc96ded96f0e9539f3
Amit Acharya <amit@nestaway.com>
2016-07-06 11:59:56 +0530

    switch multiline

--- switch.rb
+++ switch.rb
@@ -38,10 +38,26 @@ def show_day_yet_another_way(number)
   end
 end
 
+def multi_line_logic(number)
+  case number
+  when 1 then puts 'Monday'
+  when 2 then puts 'Tuesday'
+  when 3 then puts 'Wednesday'
+  when 4 then puts 'Thursday'
+  when 5 then puts 'Friday'
+              puts "Tomorrow is a weekend! Can't wait for it"
+  when (6..7) then puts 'Hurray! Weekend'
+#  when 7 then puts 'Sunday'
+  else puts 'Thats not a day!'
+  end
+end
+
 (1..8).to_a.each{ |number| show_day(number) }
 puts '*' * 100
 (1..8).to_a.each{ |number| show_day_another_way(number) }
 puts '*' * 100
 (1..8).to_a.each{ |number| show_day_yet_another_way(number) }
+puts '*' * 100
+(1..8).to_a.each{ |number| multi_line_logic(number) }
 puts '*' * 100
 
