bc08972714d4e8108f2ae023b3f74f33db014249
Amit Acharya <amit@nestaway.com>
2016-07-06 11:39:09 +0530

    and not keyword in ruby

---
+++ not.rb
@@ -0,0 +1,5 @@
+aday = 'Sunday'
+bday = 'Monday'
+working_overtime = false
+
+if aday == 'Sunday' and bday == 'Monday' and not working_overtime then puts 'Thats right' else puts 'Thats not right' end
