f9861447f95f17565d68e797db65c0c9e127d27c
Amit Acharya <amit@nestaway.com>
2016-07-14 00:15:26 +0530

    inject examples

--- inject.rb
+++ inject.rb
@@ -5,3 +5,12 @@ puts (5..10).inject {|sum, n| sum + n }
 puts (5..10).inject {|product, n|
   product * n
 }
+
+array = [['a', 'A'], ['b','B']]
+
+hash = array.inject({}) do |new_hash, (key, value)|
+  new_hash[key] = value
+  new_hash
+end
+
+puts hash
